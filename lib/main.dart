9df015eb9992b3b5458af5e832417f4d643cd2b4
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2023-04-18 20:48:40 +0530

    added template files

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: FirstPage(),));
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
